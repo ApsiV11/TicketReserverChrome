c606840672782b19f4dd56a57ab961a8122dfa6c
Aapo Laakkio <aapolaakkio@outlook.com>
2023-02-21 01:12:14 +0200

    Implement the fail-safe mechanism and improve messages

--- scripts/reserver.js
+++ scripts/reserver.js
@@ -24,7 +24,13 @@ chrome.tabs.query({currentWindow: true, active: true}, async (tabs) => {
     const result = await chrome.scripting.executeScript({target: {tabId: tabs[0].id}, func: () => {return localStorage['authorization.token']} })
     
     const auth = result[0].result.substring(1, result[0].result.length-1)
-    const id = tabs[0].url.match(/https:\/\/kide\.app\/events\/([0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12})/)[1]
+    const regexedString = tabs[0].url.match(/https:\/\/kide\.app\/events\/([0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12})/)
+
+    if(!regexedString) {
+        document.write("<h2>Please navigate to the event page an reload this script!</h2>")
+    }
+
+    const id = regexedString[1]
     document.getElementById("reserveButton").addEventListener("click", async (e) => {
         //Get value of amount field
         const amountField = document.getElementById("amount").value
@@ -47,7 +53,9 @@ chrome.tabs.query({currentWindow: true, active: true}, async (tabs) => {
         const startTimeOfSales = (await (await fetch(url, options)).json())['model']['product']['dateSalesFrom']
         const startTime = new Date(startTimeOfSales).getTime()
         const now = new Date().getTime()
+        document.write("<h2>Waiting for the sales to start</h2>")
         await sleep(startTime-now >= 0 ? startTime-now : 0)
+        document.write("<h2>Begin script</h2>")
 
         let variants, product
 
@@ -64,10 +72,14 @@ chrome.tabs.query({currentWindow: true, active: true}, async (tabs) => {
             }
         }
 
+        //Get maximum amount that can be reserved
         let maxTotalReservationsPerCheckout = product['maxTotalReservationsPerCheckout']
 
         let toCreates = []
+
+        //If there is no maximum amount, reserve every type
         if(!maxTotalReservationsPerCheckout) {
+            document.write("<p>Reservations not limited. Reserving the amount specified of every type.</p>")
             for(const v of variants) {
                 let availability = v['availability']
                 let productVariantMaximumReservableQuantity = v['productVariantMaximumReservableQuantity']
@@ -80,10 +92,12 @@ chrome.tabs.query({currentWindow: true, active: true}, async (tabs) => {
         else {
             let variant = 0
 
+            //If type is selected, reserve it
             if(type.length>0) {
                 variant = int(variantSelect)
             }
 
+            //If no type is selected but a price is specified, search for correct variant
             else if(price>0) {
                 let i = 0
                 while(i<variants.length) {
@@ -96,6 +110,7 @@ chrome.tabs.query({currentWindow: true, active: true}, async (tabs) => {
                 }
             }
 
+            //Else reserve with respect to keywords
             else if(avainsanat.length>0) {
                 let i = 0
                 while(i<variants.length) {
@@ -111,18 +126,73 @@ chrome.tabs.query({currentWindow: true, active: true}, async (tabs) => {
                 }
             }
 
-            document.write("Reservations limited. Buying variant at index" + variant)
+            document.write("<p>Reservations limited. Buying variant at index" + variant+".</p>")
             let v = variants[variant]
             let availability = v['availability']
             toCreates.push({"inventoryId": v['inventoryId'], "quantity": Math.min(amount, availability, maxTotalReservationsPerCheckout)})
         }
 
+        //Reservation
         let response2 = await makeReservation(toCreates, auth)
-        document.write("Status: "+response2.status)
+        document.write("<h3>Status: "+response2.status+"</h3>")
         if(response2.status==200) {
             chrome.tabs.reload()
         }
 
-        //TODO: Implement code starting from line 123 in Python file.
+        //If the reservation fails try every variant one at a time
+        while(!maxTotalReservationsPerCheckout && response2.status!=200) {
+            toCreates=[]
+            document.write("<h3>Error, trying again</h3>")
+            if(variantIndex == variant) {
+                variantIndex = variantIndex+1
+            }
+            let v=variants[variantIndex]
+            let availability = v['availability']
+            toCreates.push({"inventoryId": v['inventoryId'], "quantity":Math.min(amount, availability, maxTotalReservationsPerCheckout)})
+
+            //Reservation
+            let response2 = await makeReservation(toCreates, auth)
+            document.write("<h3>Status: "+response2.status+"</h3>")
+
+            if(response2.status==200) {
+                chrome.tabs.reload()
+            }
+
+            variantIndex = variantIndex+1
+        }
+
+        //If the availibity info is wrong, we need to try again without the variant that failed
+        while(maxTotalReservationsPerCheckout && response2.status!=200) {
+            document.write("<h3>Error, trying again</h3>")
+            let jsonData = response2.json()
+            let errorInventoryId = jsonData['error']['entity']['inventoryId']
+
+            let newVariants = []
+
+            for(const v of toCreates) {
+                if(variant['inventoryId']!=errorInventoryId) {
+                    newVariants.push(variant)
+                }
+            }
+
+            toCreates = newVariants
+
+            //Reservation
+            let response2 = await makeReservation(toCreates, auth)
+            document.write("<h3>Status: "+response2.status+"</h3>")
+
+            if(response2.status==200) {
+                chrome.tabs.reload()
+            }
+        }
+        //document.write("<h3>Response JSON</h3>")
+        //document.write(`<p>${JSON.stringify(await response2.json())}</p>`)
+
+        if(response2.status==200) {
+            document.write("<h2>Reservation successful!</h2>")
+        }
+        else {
+            document.write("<h2>Error!</h2>")
+        }
     })
 });
