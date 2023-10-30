5a2d2bcd194c07409134b965e1c6cf40e97feec1
Aapo Laakkio <aapolaakkio@outlook.com>
2023-10-30 23:52:30 +0200

    Make bot work with changes to Kide.app

--- scripts/reserver.js
+++ scripts/reserver.js
@@ -2,33 +2,36 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function makeReservation(toCreates, auth) {
-    let body = {"toCreate":toCreates,"toCancel":[], expectCart: true, includeDeliveryMethods: false}
-
-    const url = "https://api.kide.app/api/reservations"
-    const options = {
-        method: "POST",
-        headers: {
-            "authorization": "Bearer "+auth,
-            'origin': 'https://kide.app',
-            'referer': 'https://kide.app/',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(body)
-    }
-
-    return fetch(url, options)
+function getRequestId(inventoryId) {
+	const hash = '3a7ceab52edd4b7ba69e3e120be4793f';
+	return btoa(
+		[...inventoryId.replace(/-/g, '')]
+			.map((char, i) =>
+				String.fromCharCode(char.charCodeAt(0) ^ hash.charCodeAt(i))
+			)
+			.join('')
+	).substring(0, 8);
 }
 
-async function isSuccessful(reservationPost) {
-    try {
-        const reservationJson = await reservationPost.json()
-        const successful = reservationPost.status==200 && reservationJson?.['model']?.['reservations']?.length>0
-        return {successful, reservationJson}
-    }
-    catch {
-        return {successful: false, reservationJson: null}
-    }
+async function makeReservation(toCreates, auth) {
+    const url = "https://api.kide.app/api/reservations"
+    const successes = await Promise.allSettled(toCreates.map((toCreate) => {
+        let body = {"toCreate":[toCreate],"toCancel":[], expectCart: false, includeDeliveryMethods: false}
+        const options = {
+            method: "POST",
+            headers: {
+                "authorization": "Bearer "+auth,
+                'origin': 'https://kide.app',
+                'referer': 'https://kide.app/',
+                'Content-Type': 'application/json;charset=UTF-8',
+                'X-Requested-Id': getRequestId(toCreate['inventoryId'])
+            },
+            body: JSON.stringify(body)
+        }
+        return fetch(url, options).then((res) => res.status==200 ? toCreate['inventoryId'] : null)
+    }))
+
+    return successes.filter((s) => s.value!==null)
 }
 
 const cont = document.getElementById("container")
@@ -131,13 +134,11 @@ chrome.tabs.query({currentWindow: true, active: true}, async (tabs) => {
         //Get maximum amount that can be reserved
         let maxTotalReservationsPerCheckout = product['maxTotalReservationsPerCheckout']
 
-        let reservationPost
-        let reservationJson
         let successful = false
 
         let firstIteration = true
 
-        while(!successful) {
+        while(!successful && firstIteration) {
 
             let toCreates = []
             let variant = 0
@@ -197,16 +198,22 @@ chrome.tabs.query({currentWindow: true, active: true}, async (tabs) => {
             }
 
             //Reservation
-            reservationPost = await makeReservation(toCreates, auth)
-            addMessage("Status: "+reservationPost.status, 1)
-            const result = await isSuccessful(reservationPost)
-            successful = result['successful']
-            reservationJson = result['reservationJson']
+            let successes = await makeReservation(toCreates, auth)
+            successful = successes.length>0
+            addMessage(successful ? 'Reservation successful!' : 'Reservation failed, trying again', successful ? 0 : 1)
 
             if(successful) {
                 chrome.tabs.reload()
             }
 
+            /**
+             * The following code is redundant, but it's here if it's needed in the future
+             * Now days Kide.app requires a X-Requested-Id header to be set, which is a hash of the inventoryId
+             * Thus only one item category can be reserved at a time
+             */
+
+            /**
+            *
             let variantIndex = 0
             //If the reservation fails try every variant one at a time
             while(maxTotalReservationsPerCheckout && !successful) {
@@ -259,18 +266,18 @@ chrome.tabs.query({currentWindow: true, active: true}, async (tabs) => {
                     chrome.tabs.reload()
                 }
             }
+            *
+            **/
+
             //addMessage("Response JSON", 1)
             //addMessage(`${JSON.stringify(reservationJson)}`, 2)
 
-            if(reservationJson && successful) {
-                addMessage("Reservation successful!", 0)
-                const reservations = reservationJson['model']['reservations']
+            if(successful) {
+                const successfulVariants = successes.map((s) => variants.find((v) => v['inventoryId']==s.value))
+                const reservations = successfulVariants.map((v) => ({variantName: v['name'], reservedQuantity: toCreates.find((t) => t['inventoryId']==v['inventoryId'])['quantity']}))
                 const reservationsText = reservations.map((r) => `${r['reservedQuantity']} of ticket ${r['variantName']}`).join(', ')
                 addMessage("Reserved " + reservationsText+".", 2)
                 addMessage("This box can now be closed.", 2)
-            }
-            else {
-                addMessage("Error!", 0)
             }
             
             firstIteration = false
